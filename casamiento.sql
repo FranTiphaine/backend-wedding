f3990eb2092b2c22ca376fe05fec9bab14ef6eca
Francisco Tiphaine <franciscotiphaine@gmail.com>
2022-08-27 11:54:13 -0300

    Se realizan ajusten con la base de datos por Workwrench

--- casamiento.sql
+++ casamiento.sql
@@ -1,107 +1,105 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.30, for macos12 (x86_64)
 --
--- Servidor: localhost
--- Tiempo de generación: 15-08-2022 a las 19:38:51
--- Versión del servidor: 10.4.21-MariaDB
--- Versión de PHP: 7.4.29
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: us-cdbr-east-06.cleardb.net    Database: heroku_35c1f78d56ac00a
+-- ------------------------------------------------------
+-- Server version	5.6.50-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de datos: `casamiento`
+-- Table structure for table `invitados`
 --
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `invitados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8 */;
+CREATE TABLE `invitados` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(250) NOT NULL,
+  `apellido` varchar(250) NOT NULL,
+  `mail` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `novedades`
+-- Dumping data for table `invitados`
 --
-use heroku_35c1f78d56ac00a;
 
+LOCK TABLES `invitados` WRITE;
+/*!40000 ALTER TABLE `invitados` DISABLE KEYS */;
+INSERT INTO `invitados` VALUES (1,'Francisco','Tiphaine','francsicotiphaine@gmail.com'),(2,'Camila','Posbeyikian','camila.posbe@gmail.com');
+/*!40000 ALTER TABLE `invitados` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `novedades`
+--
+
+DROP TABLE IF EXISTS `novedades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `novedades` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `titulo` varchar(250) DEFAULT NULL,
   `subtitulo` varchar(250) DEFAULT NULL,
   `cuerpo` varchar(250) DEFAULT NULL,
-  `img_id` varchar(250) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `img_id` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `novedades`
+-- Dumping data for table `novedades`
 --
 
-INSERT INTO `novedades` (`id`, `titulo`, `subtitulo`, `cuerpo`, `img_id`) VALUES
-(1, 'La Fiesta de Casamiento', 'Se realizara el dia 29/10/2022 a las 17 hs en Estancia La Linda.', 'La ceremonia comienza puntual, por lo que solicita llegar temprano.', ''),
-(2, 'El Civil', 'Se realizara el dia 27/10/2022 a las 12 hs en san Isidro.', 'La ceremonia comienza puntual, por lo que solicita llegar temprano y con barbijo.', ''),
-(3, 'SAVE THE DATE!', 'Se enviaron tarjetas invitacion fiesta y civil.', 'Falta aun gente a confirmar.', NULL),
-(4, 'Buscar Traje Tintoreria', 'Tintoreria El Craftus, en la calle Libertador 6655.', 'Se realizó el pago de la preseña.', 'rx9yp9tnjajrgakpppu5'),
-(6, 'Estamos eligiendo la música para bailar', 'Armamos la seleccion en conjunto con el DJ de CAMISANI!', 'Y ya tenemos una preselección de playlists de Spotify.', 'dlmsphw8jv5aiwcuqdfw');
-
--- --------------------------------------------------------
+LOCK TABLES `novedades` WRITE;
+/*!40000 ALTER TABLE `novedades` DISABLE KEYS */;
+INSERT INTO `novedades` VALUES (1,'La Fiesta de Casamiento','Se realizara el dia 29/10/2022 a las 17 hs en Estancia La Linda.','La ceremonia comienza puntual, por lo que solicita llegar temprano.','djgiivfchjob9436tfzb'),(3,'SAVE THE DATE!','Se enviaron tarjetas invitacion fiesta y civil.','Falta aun gente a confirmar.','s2cbnwiq0ndnfmddywor'),(4,'Buscar Traje Tintoreria','Tintoreria El Craftus, en la calle Libertador 6655.','Se realizó el pago de la preseña.','t6eloqyvi4l8uxjjkyxs'),(6,'Estamos eligiendo la música para bailar','Armamos la seleccion en conjunto con el DJ de CAMISANI!','Y ya tenemos una preselección de playlists de Spotify.','ohnhqilgvcitkxrppmce');
+/*!40000 ALTER TABLE `novedades` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Table structure for table `usuarios`
 --
 
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `usuario` varchar(250) NOT NULL,
-  `password` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `password` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `usuarios`
+-- Dumping data for table `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `usuario`, `password`) VALUES
-(1, 'franciscotiphaine@gmail.com', '347442e3e43213c09ff3f36f39cc856b'),
-(2, 'franciscotiphaine@hotmail.com', 'd2b5a601e2d39c856c8909c74ea31092'),
-(3, 'francis@gmail.com', 'f3b7e5d3eb074cde5b76e26bc0fb5776');
-(4,'flavia.ursino@gmail.com','flavia1234');
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'franciscotiphaine@gmail.com','347442e3e43213c09ff3f36f39cc856b'),(2,'franciscotiphaine@hotmail.com','d2b5a601e2d39c856c8909c74ea31092'),(3,'francis@gmail.com','f3b7e5d3eb074cde5b76e26bc0fb5776'),(4,'flavia.ursino@gmail.com','6be9e6d801b368c7f046531892d8a917');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `novedades`
---
-ALTER TABLE `novedades`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `novedades`
---
-ALTER TABLE `novedades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-08-27 11:52:30
